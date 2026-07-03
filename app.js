be4650371e63d3453c1ae9d6973830b781a68935
shifauabdulmalik-hue <shifauabdulmalik@gmail.com>
2026-07-03 10:37:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,23 @@
-// 1. Firebase Config (replace later)
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// 2. Init Firebase
-firebase.initializeApp(firebaseConfig);
-const storage = firebase.storage();
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDEKq-V5yiiZlnkFAm509PaOjEQ2O-uae0",
+    authDomain: "shifau-explainer.firebaseapp.com",
+    projectId: "shifau-explainer",
+    storageBucket: "shifau-explainer.firebasestorage.app",
+    messagingSenderId: "95376583442",
+    appId: "1:95376583442:web:b8c2b06a872477a2a9a1cb",
+    measurementId: "G-8PZHL6XRXS"
+  };
 
-// 3. Elements
-const fileInput = document.getElementById("fileInput");
-const uploadBtn = document.getElementById("uploadBtn");
-const status = document.getElementById("status");
-
-// 4. Upload Logic
-uploadBtn.addEventListener("click", async () => {
-
-  const files = fileInput.files;
-
-  if (!files.length) {
-    status.innerText = "❌ No files selected";
-    return;
-  }
-
-  status.innerText = "Uploading... ⏳";
-
-  for (let file of files) {
-
-    const fileRef = storage.ref("uploads/" + Date.now() + "_" + file.name);
-
-    await fileRef.put(file);
-
-    const url = await fileRef.getDownloadURL();
-
-    console.log("Uploaded:", url);
-  }
-
-  status.innerText = "✅ Upload complete!";
-});
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
